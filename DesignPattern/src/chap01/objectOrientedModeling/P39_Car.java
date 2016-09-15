f32ad3b4c5fdbb4e6ddaf8887114fd8c9f663eb9
juhyun10 <juhyun1010@naver.com>
2016-09-15 18:59:59 +0900

    의존관계 (체크포인트 p.39)

---
+++ DesignPattern/src/chap01/objectOrientedModeling/P39_Car.java
@@ -0,0 +1,11 @@
+package chap01.objectOrientedModeling;
+
+/**
+ * 의존관계 (체크포인트 p.39)
+ *
+ * @author assu
+ * @date 2016. 9. 15.
+ */
+public class P39_Car {
+
+}
---
+++ DesignPattern/src/chap01/objectOrientedModeling/P39_Person.java
@@ -0,0 +1,19 @@
+package chap01.objectOrientedModeling;
+
+/**
+ * 의존관계 (체크포인트 p.39)
+ *
+ * @author assu
+ * @date 2016. 9. 15.
+ */
+public class P39_Person {
+	private P39_Car car;
+	
+	public void setCar(P39_Car car) {
+		this.car = car;
+	}
+	
+	public P39_Car getCar() {
+		return this.car;
+	}
+}
